55acbf948d4ccdc955f207e96c4be666dc8ac0f3
wFairmanOlin <will@students.olin.edu>
2018-03-09 11:53:33 -0500

    modified user interface

--- bokehMap.py
+++ bokehMap.py
@@ -1,9 +1,13 @@
 from bokeh.io import output_file, show
 from bokeh.models import (
-  GMapPlot, GMapOptions, ColumnDataSource, Circle, Range1d, PanTool, WheelZoomTool, BoxSelectTool
+  GMapPlot, GMapOptions, ColumnDataSource, CustomJS, Circle, Range1d, PanTool, WheelZoomTool, BoxSelectTool
 )
 from bokeh.layouts import widgetbox, column
 from bokeh.models.widgets import Button, RadioButtonGroup, Select, Slider
+
+
+
+
 
 
 map_options = GMapOptions(lat=42.36, lng=-71.05, map_type="roadmap", zoom=11)
@@ -20,6 +24,8 @@ plot.api_key = " AIzaSyBt2HETloZkCelX_XuVAXAgRkds_nBhNZQ"
 
 #42.2932° N, 71.2637° W
 #42.2936° N, 71.3059° W
+#data = {'olin': [42.2932, -71.2637], 'wellsley': [42.2936, -71.3059]}
+
 source = ColumnDataSource(
     data=dict(
         lat=[42.2932, 42.2936],
@@ -28,12 +34,26 @@ source = ColumnDataSource(
 )
 
 circle = Circle(x="lon", y="lat", size=15, fill_color="blue", fill_alpha=0.8, line_color=None)
+
 plot.add_glyph(source, circle)
 
 plot.add_tools(PanTool(), WheelZoomTool(), BoxSelectTool())
 
-button_group = RadioButtonGroup(labels=["Qin Mo", "Deng Qhin Mo", "Josh Deng"], active=0)
-output_file("gmap_plot.html")
-layout = column(plot, button_group)
-show(layout)
+callback = CustomJS(args=dict(p=circle, source=source), code="""
+
+            dsfdgfbjhbagahgbyukabgfyu
+            var radio_value = cb_obj.active;
+            var data = source.data;
+            x = data['lon'];
+            y = data['lat'];
+            x = x[radio_value]
+            y = y[radio_value]
+        source.change.emit();
+        """)
+
+
+button_group = RadioButtonGroup(labels=["Qin Mo", "Deng Qhin Mo", "Josh Deng"], active=0, callback=callback)
+#output_file("gmap_plot.html")
+#layout = column(plot, button_group)
+show(column(plot, button_group))
 #show(widgetbox(button_group))
