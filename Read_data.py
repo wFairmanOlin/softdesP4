3e2f951beaee9ca561d0f6179a7ca2a0e0a223cc
wFairmanOlin <will@students.olin.edu>
2018-03-14 09:15:05 -0400

    modified coordinate finding code

--- Read_data.py
+++ Read_data.py
@@ -8,9 +8,9 @@ def read_json(filename):
         data = json.load(fp)
     return data
 
-vio_percentage = read_json('analyzed_data/restaurant_violation_percentage.txt')
-print(vio_percentage)
-severity1 = read_json("analyzed_data/severity1_violation_percentage.txt")
-severity2 = read_json("analyzed_data/severity2_violation_percentage.txt")
-severity3 = read_json("analyzed_data/severity3_violation_percentage.txt")
-
+if __name__ == "__main__":
+    vio_percentage = read_json('analyzed_data/restaurant_violation_percentage.txt')
+    print(vio_percentage)
+    severity1 = read_json("analyzed_data/severity1_violation_percentage.txt")
+    severity2 = read_json("analyzed_data/severity2_violation_percentage.txt")
+    severity3 = read_json("analyzed_data/severity3_violation_percentage.txt")
