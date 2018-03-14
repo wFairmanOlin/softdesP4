987a2935dd0ff280773b046f1d917df08fa5d68f
wFairmanOlin <will@students.olin.edu>
2018-03-14 14:28:36 -0400

    found error in code. Could not fix

--- coordinateFinder.py
+++ coordinateFinder.py
@@ -1,19 +1,33 @@
 from geopy.geocoders import Nominatim
-from Data_Minng_sofdesmp4 import *
-from Read_data import *
+from Read_data import read_json, read_pickle
 import time
 
 geolocator = Nominatim()
 def add_coordinates(filename):
-    print('works')
-    results = read_json(filename)
-    for i in results:
-        print(i)
-        address = i['address']
-        location = geolocator.geocode(address)
-        i['lon'] = location.latitude
-        i['lat'] = location.longitude
-        time.sleep(100)
+    print('getting results...')
+    results = read_pickle(filename)
+    print('results returned...')
+    print(len(results))
+    print(type(results))
+    for key in results:
+        a = results[key]
+        print(key)
+        for key1 in a:
+            print(key1)
+            print(type(a['address']))
+            for key2 in a['address']:
+                print(type(key2))
+        #print(results[key])
+        pass
+        #i = results[key]
+        #print(i)
+        #address = i['address']
+        #print(address)
+        #print('next \n\n')
+        #location = geolocator.geocode(address)
+        #i['lon'] = location.latitude
+        #i['lat'] = location.longitude
+        #time.sleep(100)
 
 
 if __name__ == "__main__":
@@ -21,4 +35,4 @@ if __name__ == "__main__":
     #location = geolocator.geocode("175 5th Avenue NYC")
     #print((location.latitude, location.longitude))
     #print(location.address)
-    add_coordinates('analyzed_data/severity1_violation_percentage.txt')
+    add_coordinates("analyzed_data/severity1_violation_percentage.pickle")
